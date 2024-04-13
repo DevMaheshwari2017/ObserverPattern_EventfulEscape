888092bacf83f073efed041bed80ea7516c8e868
Dev <devmaheswari2017@gmail.com>
2024-04-13 15:23:05 +0530

    added GhostLightOff Event

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int KeysRequiredToTrigger;
+    [SerializeField] SoundType soundType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<PlayerView>() != null && KeysRequiredToTrigger == GameService.Instance.GetPlayerController().KeysEquipped) 
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GameService.Instance.GetSoundView().PlaySoundEffects(soundType);
+        }
+    }
 
 }
